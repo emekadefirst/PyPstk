15b4026169b5e72148167d2dc85ef311054f54b8
President Carter <olabodebalogun80@gmail.com>
2024-03-14 00:04:31 +0100

    ok

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as f:
     description = f.read()
 
 setup(
-    name="Pystack",
+    name="Paystack-Python-2-SDK",
     version="1.0.0",
     long_description= description,
     long_description_content_type= "text/markdown",
     author="Victor Chibuogwu Chukwuemeka aka Emekadefirst",
     author_email="emekadefirst@gmail.com",
-    url="https://github.com/emekadefirst/Pystack",
+    url="https://github.com/emekadefirst/Paystack-Python-2-SDK",
     packages=find_packages(),
     install_requires=[
         "requests>=2.26.0",
